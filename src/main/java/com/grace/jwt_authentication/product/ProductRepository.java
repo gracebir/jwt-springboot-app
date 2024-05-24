bcce5d4db527164c43cb20f10fb6f3a6b1c98969
gracebir <grace.devolop@gmail.com>
2024-05-24 22:05:02 +0200

    user and product repository

---
+++ src/main/java/com/grace/jwt_authentication/product/ProductRepository.java
@@ -0,0 +1,4 @@
+package com.grace.jwt_authentication.product;
+
+public interface ProductRepository {
+}
---
+++ src/main/java/com/grace/jwt_authentication/user/UserRepository.java
@@ -0,0 +1,4 @@
+package com.grace.jwt_authentication.user;
+
+public interface UserRepository {
+}
