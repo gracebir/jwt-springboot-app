f46291b80450399d66d20b95ca1554b8369d710b
gracebir <grace.devolop@gmail.com>
2024-05-24 22:05:16 +0200

    user and product repository

--- src/main/java/com/grace/jwt_authentication/product/ProductRepository.java
+++ src/main/java/com/grace/jwt_authentication/product/ProductRepository.java
@@ -1,4 +1,6 @@
 package com.grace.jwt_authentication.product;
 
-public interface ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
--- src/main/java/com/grace/jwt_authentication/user/UserRepository.java
+++ src/main/java/com/grace/jwt_authentication/user/UserRepository.java
@@ -1,4 +1,6 @@
 package com.grace.jwt_authentication.user;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
